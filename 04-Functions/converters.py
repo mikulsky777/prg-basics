17a4b6e2deefc0b954673b179e16e1e55e6afa6a
Mikolaj Tomasiak <mikolaj.tomasiak2137@gmail.com>
2024-10-22 14:22:47 +0200

    zadania 5 nieskonczone

--- 04-Functions/converters.py
+++ 04-Functions/converters.py
@@ -4,6 +4,12 @@ def m_to_cm(n):
 def cm_to_m(n):
     return n/100
 
+def cm_to_in(n):
+    return n/2.54
+
+def ft_and_in_to_cm(n,m):
+    return n*30.48+m*2.54
+
 if __name__ == "__main__":
     # only execute when you run this module
     # so you can test the functions in this place
--- 04-Functions/test_converters.py
+++ 04-Functions/test_converters.py
@@ -2,5 +2,6 @@ import converters
 
 print('### Test converters')
 print(f'Three meters is {converters.m_to_cm(3)}cm')
+print(f'Three cm are {converters.cm_to_in(3)} inches')
 
 
