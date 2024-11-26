bc30f215c13c5b4af17760bbe4baa039e483b5dc
Mikolaj Tomasiak <mikolaj.tomasiak2137@gmail.com>
2024-11-26 14:24:26 +0100

    zadania 3 nieskonczone

--- 09-DictionariesStacksAndQueues/stack_example.py
+++ 09-DictionariesStacksAndQueues/stack_example.py
@@ -12,19 +12,27 @@ on the top is the first one you'll take off.
 # creates a stack
 cards = queue.LifoQueue()
 
-# adds elements to the top of the stack
-cards.put('King of Hearts \u2665')    
-cards.put('Queen of Diamonds \u2666')  
-cards.put('Jack of Spades \u2660')     
+# adds elements to the top of the stack  
+cards.put(2)
+cards.put(3)
+cards.put(4)
+cards.put(1)
+cards.put(9)
+cards.put(8)
 
 ## prints number of elements of the stack
 print('Number of stack elements:', cards.qsize())
 
+sum = cards.get() + cards.get()
+print(f'sum: {sum}')
+
+sum = 0
+
 # removes and prints elements from the top of the stack
 while not cards.empty():
-    card = cards.get()
-    print(card)
+    sum += cards.get()
 
+print(f'sum of the remaining stack elements: {sum}')
 """
 Note the order of the printed elements.
 The last added element is printed first.
